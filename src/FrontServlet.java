c363a459ffd7ce40b30eea873bef7db2807a6d69
DydiRah <124720797+DydiRah@users.noreply.github.com>
2023-03-21 18:05:36 +0100

    Add files via upload

--- src/FrontServlet.java
+++ src/FrontServlet.java
@@ -10,11 +10,39 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import etu1984.framework.Mapping;
+import etu1984.framework.annot.UrlA;
 
 public class FrontServlet extends HttpServlet{
 
     HashMap<String,Mapping> mappingUrls = new HashMap<>();
 
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        try {
+            mappAllClass(getInitParameter("package"));
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    public void mappAllClass(String pack) throws Exception{ 
+        File dir = new File(pack);
+        File[] files = dir.listFiles();
+        Mapping temp = new Mapping();
+        for (File file : files) {
+            String nameClass = file.getName().replace(".class","");
+            Class<?> c = Class.forName("modele."+nameClass);
+            Method[] ms = c.getDeclaredMethods();
+            for (Method m : ms) {
+                if(m.isAnnotationPresent(UrlA.class)) {
+                    temp.setClassName(nameClass);
+                    temp.setMethod(m.getName());
+                    this.mappingUrls.put(m.getAnnotation(UrlA.class).url(), temp);
+                }
+            }
+        }
+    }
 
     public void getUrl(PrintWriter out,HttpServletRequest req, HttpServletResponse resp) {
         String requete = req.getRequestURI();
@@ -38,6 +66,9 @@ public class FrontServlet extends HttpServlet{
         PrintWriter out = resp.getWriter();
         try {        
             getUrl(out, req, resp);
+            this.mappingUrls.forEach((key, value) -> {
+                out.print(key + " : " + value.getClassName() + " , " + value.getMethod());  
+            });
         } catch (Exception e) {
             out.println(e.getMessage());
         }
