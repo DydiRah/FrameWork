2961b52aa688f1dd6e93b2c664040d2c4fe06c3e
DydiRah <124720797+DydiRah@users.noreply.github.com>
2023-04-22 13:02:24 +0200

    Add files via upload

--- Framework/src/ModelView.java
+++ Framework/src/ModelView.java
@@ -1,7 +1,30 @@
 package etu1984.framework;
 
+import java.util.HashMap;
+
 public class ModelView {
     String url;
+    HashMap<String,Object> data = new HashMap<>();
+
+    public ModelView(String url){
+        setUrl(url);
+    }
+    public ModelView(String url,HashMap<String,Object> data){
+        setUrl(url);
+        setData(data);
+    }
+
+    public void addItem(String key,Object value){
+        getData().put(key, value);
+    }
+
+    public HashMap<String, Object> getData() {
+        return data;
+    }
+
+    public void setData(HashMap<String, Object> data) {
+        this.data = data;
+    }
 
     public String getUrl() {
         return url;
@@ -11,8 +34,5 @@ public class ModelView {
         this.url = url;
     }
 
-    public ModelView(String url){
-        setUrl(url);
-    }
     
 }
